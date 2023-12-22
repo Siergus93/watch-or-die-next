d06bc0c087fffd90e5948f2f02e4d85cd4fb2523
Siergus <sirrotto@gmail.com>
2023-12-22 20:42:06 +0100

    Added movies list

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,13 +1,20 @@
 import { sql } from "@vercel/postgres";
 import { unstable_noStore as noStore } from "next/cache";
 
+export type Movie = {
+  id: string;
+  title: string;
+  poster_image_url: string;
+  release_date: Date;
+};
+
 export async function fetchMovies() {
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
 
   try {
-    const data = await sql`SELECT * FROM movies`;
+    const data = await sql<Movie>`SELECT * FROM movies`;
     return data.rows;
   } catch (error) {
     console.error("Database Error:", error);
--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -6,21 +6,52 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  const movies = await fetchMovies();
-  console.log(movies);
+  const initialMovies = await fetchMovies();
+  const movies = [...initialMovies, ...initialMovies, ...initialMovies];
+  console.log("movies", movies);
+  console.log(
+    "movies[0].release_date",
+    movies[0].release_date.getUTCFullYear()
+  );
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <p>Movies page</p>
-      <ul>
-        {movies.map((movie) => {
-          return (
-            <li key={movie.id}>
-              <p>{movie.title}</p>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Customers also purchased
+          </h2>
+
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {movies.map((movie) => (
+              <div key={movie.id} className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                  <img
+                    src={movie.poster_image_url}
+                    alt={movie.title}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <a href={movie.poster_image_url}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {movie.title}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {movie.release_date.getUTCFullYear()} - Horror
+                    </p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">8.4/10</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
